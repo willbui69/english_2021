d5fa68d3aa466e496120eda918ea92a4c93b2c6f
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-12-12 13:59:00 +1100

    Import week_notes.scss file to index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,12 @@
 import { addSidebarTemplate } from './js/sidebar-template.js';
 import './styles/base.scss'
 import './styles/navi.scss'
+import './styles/week_notes.scss'
 
+//Add sidebar template to the index.html
 addSidebarTemplate();
+
+
 
 export {
     addSidebarTemplate
