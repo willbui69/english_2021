8f37f4d843b96ebcbe35c098aec3355205718b46
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-12-12 12:47:42 +1100

    Set up the server

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const fetch = require('node-fetch')
+const dotenv = require('dotenv')
+dotenv.config();
+/* Middleware*/
+const bodyParser = require('body-parser');
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+console.log(__dirname)
+
+// Cors for cross origin allowance
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
+
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8000, function() {
+    console.log('The English app listening on port 8000');
+})
