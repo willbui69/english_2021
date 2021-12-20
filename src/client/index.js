64cd1cfd127445d4be5e0fcc89f2d07ebe870827
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-12-20 19:52:09 +1100

    Update the index.js file

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,11 @@
 import { addSidebarTemplate } from './js/sidebarTemplate.js';
-import { fixNaviBar } from './js/horizonBarFixed.js';
-import { toTheTop } from './js/scrollToTopButton.js';
 import './styles/base.scss'
 import './styles/navi.scss'
-import './styles/week_notes.scss'
 import './styles/lesson.scss'
 
 //Add sidebar template to the index.html
 addSidebarTemplate();
-document.addEventListener('DOMContentLoaded', toTheTop);
 
 export {
-    addSidebarTemplate,
-    fixNaviBar,
-    toTheTop
+    addSidebarTemplate
 }
