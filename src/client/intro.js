87cbf3508cc29e1f0671048fe36d7872bb71d8b9
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-12-21 22:56:52 +1100

    Rename the index.js to intro.js

---
+++ src/client/intro.js
@@ -0,0 +1,11 @@
+import { addSidebarTemplate } from './js/sidebarTemplate.js';
+import './styles/base.scss'
+import './styles/navi.scss'
+import './styles/lesson.scss'
+
+//Add sidebar template to the index.html
+addSidebarTemplate();
+
+export {
+    addSidebarTemplate
+}
