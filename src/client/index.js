e25ff94f1a2c2abcfeda94a666caaab6480e41aa
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-12-23 18:15:43 +1100

    Validate user credentials

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
+import { checkUserCreds } from './js/handleUserCredentials';
 import './styles/index.scss'
 import google from './media/google.png'
 import background from './media/background.png'
 
+document.getElementById('loginBtn').addEventListener('click', checkUserCreds);
 
-
+export {
+    checkUserCreds
+}
