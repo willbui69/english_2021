2d9fdb31780606ae9a5cb5627621a9683d3c3abc
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-12-24 23:18:55 +1100

    Set up google authentication

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const fetch = require('node-fetch')
 const dotenv = require('dotenv')
+const mongo = require('mongodb')
 dotenv.config();
 /* Middleware*/
 const bodyParser = require('body-parser');
