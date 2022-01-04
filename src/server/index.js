6af8115bd55b56e65fe4473199e11a943c443c9c
willbui69 <76840668+willbui69@users.noreply.github.com>
2022-01-04 21:19:23 +1100

    Configure index js file

--- src/server/index.js
+++ src/server/index.js
@@ -4,20 +4,13 @@ const app = express()
 const fetch = require('node-fetch')
 const dotenv = require('dotenv')
 const mongo = require('mongodb')
+const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 dotenv.config();
-
-//Google Authentication
-const {OAuth2Client} = require('google-auth-library');
-const CLIENT_ID = '177921256407-jfh7lklk7ttmj69a7l98817hvm6ipl92.apps.googleusercontent.com';
-const client = new OAuth2Client(CLIENT_ID);
 
 /* Middleware*/
 const bodyParser = require('body-parser');
 app.use(cookieParser());
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('views', '/Users/mac/Desktop/english-2021/src/client/views');
 //Data project endpoint
 const projectData = {};
 
@@ -25,7 +18,6 @@ const projectData = {};
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 console.log(__dirname)
-
 
 // Cors for cross origin allowance
 const cors = require('cors');
@@ -45,77 +37,9 @@ app.listen(8080, function() {
 
 //Get user login-credentials
 app.post('/postuserlogindata', async(req, res)=>{
-    try {
-        //Save user credentials in server side
-        projectData["userName"] = await req.body.userName;
-        projectData["passWord"] = await req.body.passWord;
-        console.log(projectData);
-
-        //Check user credentials are valid
-        validateUserLogin(projectData["userName", "passWord"])
-        .then(()=>{
-            //Send response to client side
-
-        })
-    }catch(error){
-        console.log("error", error);
-    }
+    console.log(req.body);
+    res.json({ status: 'ok'})
 })
-
-//Get Log in route from google accounts
-app.post('/login', (req, res)=>{
-    let token = req.body.token;
-    console.log(token);
-
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: CLIENT_ID, 
-        });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
-        console.log(payload);
-      }
-      verify()
-      .then(()=>{
-        res.cookie('session-token', token);
-        res.send('success');
-      }).
-      catch(console.error);
-})
-
-app.get('/startpage', checkAuthenticated, (req, res)=>{
-    let user = req.user;
-    res.render('startpage.html', {user});
-})
-
-app.get('/logout', (req, res)=>{
-    res.clearCookie('session-token');
-    res.redirect('/');
-})
-
-function checkAuthenticated(req, res, next){
-    let token = req.cookies['session-token'];
-    let user = {};
-    async function verify(){
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: CLIENT_ID,
-        });
-        const payload = ticket.getPayload();
-        user.name = payload.name;
-        user.email = payload.email;
-        user.picture = payload.picture;
-    }
-    verify()
-    .then(()=>{
-        req.user = user;
-        next();
-    })
-    .catch(err=>{
-        res.redirect('/')
-    })
-}
 
 //Validate user credientials
 const validateUserLogin = async (userName, passWord)=>{
