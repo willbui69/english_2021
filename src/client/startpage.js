a1fbc7bfd1350c3492d7d9f82e229d27a5fd38d6
willbui69 <76840668+willbui69@users.noreply.github.com>
2022-01-04 21:16:46 +1100

    Configure startpage js file

--- src/client/startpage.js
+++ src/client/startpage.js
@@ -1,6 +1,7 @@
 import { addSidebarTemplate } from './js/sidebarTemplate.js';
 import './styles/base.scss'
 import './styles/navi.scss'
+import './styles/topic.scss'
 import './styles/lesson.scss'
 
 //Add sidebar template to the startpage.html
