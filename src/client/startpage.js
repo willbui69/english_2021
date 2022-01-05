e16b1ec96927efc805977853f83fa8c2e6e2a9f0
willbui69 <76840668+willbui69@users.noreply.github.com>
2022-01-06 00:16:38 +1100

    Add scss file for startpage

--- src/client/startpage.js
+++ src/client/startpage.js
@@ -3,6 +3,7 @@ import './styles/base.scss'
 import './styles/navi.scss'
 import './styles/topic.scss'
 import './styles/lesson.scss'
+import './styles/startpage.scss'
 
 //Add sidebar template to the startpage.html
 addSidebarTemplate();
