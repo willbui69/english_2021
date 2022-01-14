b6fbec665b1d9263c89ff3f5f5019ff4bcea786e
willbui69 <76840668+willbui69@users.noreply.github.com>
2022-01-14 16:46:58 +1100

    Make sidebar responsive

--- src/client/startpage.js
+++ src/client/startpage.js
@@ -8,6 +8,14 @@ import './styles/startpage.scss'
 //Add sidebar template to the startpage.html
 addSidebarTemplate();
 
+const toggleButton = document.getElementById('toggle-btn')
+const naviItems = document.getElementsByClassName('navi-items')[0]
+
+toggleButton.addEventListener('click', ()=> {
+    naviItems.classList.toggle('active')
+}
+)
+
 export {
     addSidebarTemplate
 }
