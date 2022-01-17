d7bd37c83d7326ee5f13fecf13d8ea3a9ef88de9
willbui69 <76840668+willbui69@users.noreply.github.com>
2022-01-18 09:47:35 +1100

    Turn intro page into index page

--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,11 @@ const express = require('express')
 const app = express()
 const fetch = require('node-fetch')
 const dotenv = require('dotenv')
-const mongo = require('mongodb')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
+
 dotenv.config();
 
 /* Middleware*/
 const bodyParser = require('body-parser');
-app.use(cookieParser());
-//Data project endpoint
-const projectData = {};
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,18 +30,3 @@ app.listen(8080, function() {
     console.log('The English app listening on port 8080');
 })
 
-//Get user login-credentials
-app.post('/postuserlogindata', async(req, res)=>{
-    console.log(req.body);
-    res.json({ status: 'ok'})
-})
-
-//Validate user credientials
-const validateUserLogin = async (userName, passWord)=>{
-    if(userName == 'fff' && passWord == "12"){
-        window.location = "startpage.html";
-        return false;
-    }else {
-        
-    }
-}
